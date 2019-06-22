e900b2b8c4ec4045cacba6eabf8b577375f67bd5
Preeti Sharma <v-preeti.sharma@btpn.com>
2019-06-22 18:22:42 +0700

    Added search bar for filtering category wise news

--- src/Containers/NewsWithCategoryMenu.js
+++ src/Containers/NewsWithCategoryMenu.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import {Text, StyleSheet, Image, FlatList, View} from "react-native";
+import { SearchBar } from 'react-native-elements';
 import NewsItemWithoutCategory from "../Components/NewsItemWithoutCategory";
 import CategoryMenu from "../Components/CategoryMenu";
 
 export const ALL_NEWS = [
   {title: "title1", description: "D", category: "entertainment"},
   {title: "title2", description: "D", category: "entertainment"},
-  {title: "title3", description: "D", category: "entertainment"},
+  {title: "title3", description: "D", category: "science"},
   {title: "title4", description: "D", category: "entertainment"}
 ];
 
@@ -19,14 +20,38 @@ const getUniqueCategories = (news) => {
   return news.map((item) => item.category).filter(unique);
 };
 
+
 export default class NewsWithCategoryMenu extends Component<Props> {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  filterNewsByCategory = () => {
+    if(this.state.search){
+      return ALL_NEWS.filter((news) => {
+        return news.category.toLowerCase() == this.state.search.toLowerCase();
+      })
+    }
+    return ALL_NEWS;
+  };
+
   render() {
+    const { search } = this.state;
     return (
       <View style={styles.container}>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
         <CategoryMenu categories={getUniqueCategories(ALL_NEWS)}/>
         <FlatList
           testID="newsList"
-          data={ALL_NEWS}
+          data={this.filterNewsByCategory()}
           renderItem={({item}) => <NewsItemWithoutCategory news={item}/>}
           keyExtractor={(item, index) => item.title}
         />
