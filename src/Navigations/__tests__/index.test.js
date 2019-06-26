7309338a429fb4c6e4d9ee54aba39e34abfd7ad6
Ranu Wijaya Putra <ranu.putra@btpn.com>
2019-06-26 11:17:51 +0700

    add test for app container

---
+++ src/Navigations/__tests__/index.test.js
@@ -0,0 +1,18 @@
+import AppNavigator from '../'
+import NewsScreen from '../../Containers/NewsScreen'
+import DrawerStack from '../DrawerStack'
+
+describe('Navigations', () => {
+  it('should initial to NewsScreen', () => {
+    const state = AppNavigator.router.getStateForAction('Navigation/INIT')
+    const InitScreen = AppNavigator.router.getComponentForState(state)
+
+    expect(InitScreen).toEqual(NewsScreen)
+  })
+
+  it('should has DrawerStack route', () => {
+    const Screen = AppNavigator.router.getComponentForRouteName('DrawerStack')
+
+    expect(Screen).toEqual(DrawerStack)
+  })
+})
